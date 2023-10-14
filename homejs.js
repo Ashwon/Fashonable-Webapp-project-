1e9500a22df9f8b2a48ec59db4f461c677a7e766
Ashwin Sivakumar <120635800+Ashwon@users.noreply.github.com>
2023-10-14 23:22:58 +0530

    first commit!

---
+++ homejs.js
@@ -0,0 +1,10 @@
+function hoverzoom(id){
+    img = document.getElementById(id)
+    img.style.scale = 1.05; 
+}
+
+function hoverzoomout(id){
+    img = document.getElementById(id)
+    img.style.scale = 1;
+}
+
